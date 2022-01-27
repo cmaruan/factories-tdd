5b87af99f5541065cad253d87782827dcb8d4bbc
Cristian Bosin <cmaruanbosin@gmail.com>
2022-01-27 19:28:43 -0300

    Minimalist unit test

---
+++ main.c
@@ -0,0 +1,14 @@
+
+
+void test_can_create_product(void)
+{
+    // void *product = factories->product->create();
+    // ASSERT(product->primary_key > 0);
+}
+
+
+int main(void)
+{
+    test_can_create_product();
+    return 0;
+}
