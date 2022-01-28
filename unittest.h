e51c5e3342559cc43ccbdac156ca9159253ee2e8
Cristian Bosin <cmaruanbosin@gmail.com>
2022-01-28 15:14:39 -0300

    Returning the number of failed tests

--- unittest.h
+++ unittest.h
@@ -3,7 +3,7 @@
 
 #define ASSERT(expr)    if(!(expr)) { _test_failed += 1; return; }
 #define TEST_INIT()     do { _test_failed = 0; } while (0)
-#define TEST_FAILED()   (_test_failed > 0)
+#define TEST_FAILED()   (_test_failed)
 
 static int _test_failed = 0;
 
