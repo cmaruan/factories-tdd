21608cafa541d1ff631b167dae6fe545aaf75f5d
Cristian Bosin <cmaruanbosin@gmail.com>
2022-01-28 15:13:09 -0300

    We dont segfault anymore

--- factories/product_factory.c
+++ factories/product_factory.c
@@ -1,13 +1,19 @@
 #include "factories/product_factory.h"
 
 void *ProductFactory_create();
+void ProductFactory_destroy(void **);
 
 struct ProductFactory _product_factory = {
-    .create = ProductFactory_create
+    .create = ProductFactory_create,
+    .destroy = ProductFactory_destroy,
 };
 
 void *ProductFactory_create() {
     static product_t product;
     product.primary_key = 1;
     return &product;
+}
+
+void ProductFactory_destroy(void **product_ptr) {
+    *product_ptr = NULL;
 }
