fc1a3b7fee33454e762e2cf46d32672d3479e436
Cristian Bosin <cmaruanbosin@gmail.com>
2022-01-28 15:18:30 -0300

    First step towards OO

--- main.c
+++ main.c
@@ -5,17 +5,18 @@
 
 void test_can_create_product(void)
 {
-    product_t *product = factories->product->create();
+    void *product = factories->product->create();
     ASSERT(product != NULL)
-    ASSERT(product->primary_key > 0);
+    ASSERT(ProductPrimaryKey(product) != 0);
 }
 
 void test_can_destroy_product(void)
 {
-    product_t *product = factories->product->create();
-    factories->product->destroy((void **)&product);
+    void *product = factories->product->create();
+    factories->product->destroy(&product);
     ASSERT(product == NULL);
 }
+
 
 
 int main(void)
