432be394bb9a9e4349f503ec5d36eb09e855454a
Cristian Bosin <cmaruanbosin@gmail.com>
2022-01-28 16:13:59 -0300

    product is now a type

--- include/models/product.h
+++ include/models/product.h
@@ -2,8 +2,12 @@
 #define _PRODUCT_H_
 
 #include <stddef.h>
+#include "type.h"
+
+extern struct ClassType *ProductType;
 
 size_t ProductPrimaryKey(void *product);
+int ProductSetPrimaryKey(void *_product, size_t pk);
 
 
 #endif // _PRODUCT_H_
--- models/product.c
+++ models/product.c
@@ -1,11 +1,26 @@
 #include "models/product.h"
 
 typedef struct {
+    struct ClassType *type;
     size_t primary_key;
 } product_t;
+
+static struct ClassType _ProductType = {
+    .size = sizeof(product_t),
+    .ctor = NULL,
+    .dtor = NULL,
+};
+struct ClassType *ProductType = &_ProductType;
 
 size_t ProductPrimaryKey(void *_product)
 {
     product_t *product = _product;
     return product ? product->primary_key : 0U;
+}
+
+int ProductSetPrimaryKey(void *_product, size_t pk)
+{
+    product_t *product = _product;
+    product->primary_key = pk;
+    return 1;
 }
