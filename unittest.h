d33414b26b124e7db34e6c794b037e5cd4393a68
Cristian Bosin <cmaruanbosin@gmail.com>
2022-01-29 13:32:55 -0300

    Improved unittests header

--- unittest.h
+++ unittest.h
@@ -1,10 +1,36 @@
 #if !defined(_UNITTESTS_H_)
 #define _UNITTESTS_H_
 
-#define ASSERT(expr)    if(!(expr)) { _test_failed += 1; return; }
-#define TEST_INIT()     do { _test_failed = 0; } while (0)
-#define TEST_FAILED()   (_test_failed)
+#include <stdio.h>
+
+#define ASSERT(expr)                                               \
+    do                                                             \
+    {                                                              \
+        if (!UnittestValidate(#expr, !!(expr), __FILE__, __LINE__)) \
+            return;                                                \
+    } while (0)
+#define TEST_INIT()       \
+    do                    \
+    {                     \
+        _test_failed = 0; \
+    } while (0)
+#define TEST_FAILED() (_test_failed)
 
 static int _test_failed = 0;
+
+int UnittestValidate(
+    const char *expr,
+    int result,
+    const char *file,
+    int line)
+{
+    if (result == 0)
+    {
+        fprintf(stderr, "Assertion failed (%s:%d): `%s`\n", file, line, expr);
+        _test_failed += 1;
+        return 0;
+    }
+    return 1;
+}
 
 #endif // _UNITTESTS_H_
